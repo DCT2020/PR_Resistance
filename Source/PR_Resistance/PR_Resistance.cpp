b6bc87ad1eaea1a7725d4f6d7c5035a73294dc29
kimhanil <khlj752@naver.com>
2020-09-06 13:42:10 +0900

    초기 커밋 메시지입니다.

---
+++ Source/PR_Resistance/PR_Resistance.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PR_Resistance.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PR_Resistance, "PR_Resistance" );
+ 
---
+++ Source/PR_Resistance/PR_ResistanceGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PR_ResistanceGameMode.h"
+#include "PR_ResistanceCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APR_ResistanceGameMode::APR_ResistanceGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
