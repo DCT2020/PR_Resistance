6c07916890786ebbf6abcd05d3727759eac1c403
kimhanil <khlj752@naver.com>
2020-10-26 21:27:54 +0900

    re

--- Source/PR_Resistance/PR_Resistance.Build.cs
+++ Source/PR_Resistance/PR_Resistance.Build.cs
@@ -8,6 +8,32 @@ public class PR_Resistance : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "TcpSocketPlugin" });
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"Sockets",
+				"Networking"
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+
 	}
 }
